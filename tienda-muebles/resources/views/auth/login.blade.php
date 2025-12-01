2ede148232562e08d40b01bfb52196bc74c77927
Marisapr2006 <marisapenarey@gmail.com>
2025-12-01 20:11:30 +0000

    añadiendo vista login

--- tienda-muebles/resources/views/auth/login.blade.php
+++ tienda-muebles/resources/views/auth/login.blade.php
@@ -4,6 +4,69 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <style>
+        * {
+            box-sizing: border-box;
+            margin: 0;
+            padding: 0;
+            font-family: 'Arial', sans-serif;
+        }
+
+        body {
+            height: 100vh;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            background-color: #976f47;
+            color: #fff;
+        }
+
+        h1 {
+            text-align: center;
+            margin-bottom: 20px;
+            font-size: 2.5rem;
+        }
+
+        form {
+            background-color: #4b3828;
+            padding: 30px 40px;
+            border-radius: 10px;
+            box-shadow: 0 8px 20px #33261ba8;
+            width: 100%;
+            max-width: 400px;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 5px;
+            font-weight: bold;
+        }
+
+        input {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 20px;
+            border: none;
+            border-radius: 5px;
+        }
+
+        button {
+            width: 100%;
+            padding: 12px;
+            background-color: #7c542d;
+            border: none;
+            border-radius: 5px;
+            color: #fff;
+            font-size: 1rem;
+            cursor: pointer;
+            transition: background-color 0.3s ease;
+        }
+
+        button:hover {
+            background-color: #7e4c1a;
+        }
+    </style>
 </head>
 <body>
     <h1>Login</h1>
