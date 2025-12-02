59eba0b58e01525ff47810317343f9472041f606
Marisapr2006 <marisapenarey@gmail.com>
2025-12-02 23:11:38 +0000

    carrito

--- tienda-muebles/resources/views/carrito/index.blade.php
+++ tienda-muebles/resources/views/carrito/index.blade.php
@@ -199,7 +199,7 @@
         @if (empty($carrito))
             <div>
                 <p>El carro está vacío</p>
-                <a href="{{ route('productos.index') }}">
+                <a href="{{ route('productos.show') }}">
                     <i></i>Ir a la Tienda
                 </a>
             </div>
