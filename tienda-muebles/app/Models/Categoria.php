12108a544f4fe19a4e24f3fe6fc3b8ec976bf29d
Gabriel22SM <114594285+Gabriel22SM@users.noreply.github.com>
2025-12-02 00:54:44 +0000

    Add productos relationship to Categoria model

--- tienda-muebles/app/Models/Categoria.php
+++ tienda-muebles/app/Models/Categoria.php
@@ -7,5 +7,8 @@ use App\Models\Producto;
 
 class Categoria extends Model
 {
+    public function productos()
+    {
     return $this->belongsToMany(Producto::class, 'categoria_producto', 'categoria_id', 'producto_id');
+    }
 }
