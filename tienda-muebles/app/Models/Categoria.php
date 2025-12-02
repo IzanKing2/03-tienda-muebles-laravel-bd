4b49a2bc2c61e27c908aaadb6d3af8491b706332
Gabriel22SM <114594285+Gabriel22SM@users.noreply.github.com>
2025-12-02 00:51:31 +0000

    Add belongsToMany relationship with Producto model

--- tienda-muebles/app/Models/Categoria.php
+++ tienda-muebles/app/Models/Categoria.php
@@ -3,8 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Producto;
 
 class Categoria extends Model
 {
-    //
+    return $this->belongsToMany(Producto::class, 'categoria_producto', 'categoria_id', 'producto_id');
 }
